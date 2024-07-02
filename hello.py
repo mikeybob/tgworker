fbf02470770e0912c2fb43428644f30829cc1cfb
little2 <agile@little2.net>
2024-07-03 00:19:53 +0800

    0703

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from telethon import TelegramClient, sync
 import os
 from vendor.class_bot import LYClass  # 导入 LYClass
+import asyncio
+import time
 
 # 检查是否在本地开发环境中运行
 if not os.getenv('GITHUB_ACTIONS'):
@@ -11,7 +13,8 @@ if not os.getenv('GITHUB_ACTIONS'):
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 phone_number = os.getenv('PHONE_NUMBER')
-session_name = api_id + 'session_name'  # 确保与上传的会话文件名匹配
+session_name = f"{api_id}_session_name"  # 确保与上传的会话文件名匹配
+
 
 # 创建客户端
 client = TelegramClient(session_name, api_id, api_hash)
@@ -22,20 +25,31 @@ ly_class_instance = LYClass('text')
 # 定义消息处理函数
 async def process_message(message):
     try:
-        print(f"Processing message: {message.id}")
+        await asyncio.sleep(3)
+        # print(f"Processing message: {message}\n")
+        print(f"Processing message: {message.id}\n")
         if message.reply_to:
-            print(f"Message has reply_to: {message.reply_to.reply_to_msg_id}")
-        if message.reply_to and message.reply_to.reply_to_msg_id == 1:
-            ly_class_instance.ab(message)
-        elif message.reply_to and message.reply_to.reply_to_msg_id == 2:
-            ly_class_instance.cd(message)
-        elif message.reply_to and message.reply_to.reply_to_msg_id == 50047:
-            print("Calling datapan method...")
-            await ly_class_instance.datapan(client, message)  # 传入 client 实例和 message
+            print(f"Message has reply_to: {message.reply_to.reply_to_msg_id}\n")
+
+        message_handlers = {
+            50074: ly_class_instance.datapan,
+            43058: ly_class_instance.mediabk,
+            43064: ly_class_instance.showfiles,
+            43060: ly_class_instance.blgg
+        }
+
+        reply_id = message.reply_to.reply_to_msg_id if message.reply_to else None
+        handler = message_handlers.get(reply_id)
+
+        if handler:
+            if asyncio.iscoroutinefunction(handler):
+                await handler(client, message)
+            else:
+                handler(message)
         else:
-            print(f"Unhandled thread ID: {message.reply_to.reply_to_msg_id if message.reply_to else 'None'} - Message: {message.sender_id}, {message.text}")
+            print(f"Unhandled thread ID: {message.reply_to.reply_to_msg_id if message.reply_to else 'None'} - Message: {message.sender_id}, {message.text}\n")
     except Exception as e:
-        print(f"An error occurred while processing message: {e}")
+        print(f"An error occurred while processing message: {e}\n")
 
 async def main():
     await client.start(phone_number)
@@ -43,12 +57,25 @@ async def main():
     # 使用指定的 chat_id
     chat_id = -1001717350482
 
-    
+    start_time = time.time()
+    while True:
+        # 获取最近5条消息
+        async for message in client.iter_messages(chat_id, limit=5):
+            if message.reply_to:
+                await process_message(message)
+        
+        # 检查累计执行时间是否超过5分钟
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 280:  # 300秒等于5分钟
+            print("Execution time exceeded 5 minutes. Stopping.")
+            break
+        else:
+            print("Execution time is "+str(elapsed_time)+" seconds. Continuing...")
+        
+        await asyncio.sleep(15)  # 间隔15秒
 
-    # 获取最近100条消息
-    async for message in client.iter_messages(chat_id, limit=3):
-        if message.reply_to:
-            await process_message(message)
+    
+            
 
 with client:
     client.loop.run_until_complete(main())
--- vendor/class_bot.py
+++ vendor/class_bot.py
@@ -1,3 +1,5 @@
+from telethon import types
+
 class LYClass:
     def __init__(self, name):
         self.name = name
@@ -5,21 +7,99 @@ class LYClass:
     def greet(self, message):
         print(f"Hello, {self.name}!")
 
+    async def wpbot(self, client, message, bot_username):
+        try:
+            message_a_id = message.id
+            peer_a_id = message.peer_id;
+            async with client.conversation(bot_username) as conv:
+                # 发送消息到机器人
+
+                forwarded_message = await conv.send_message(message.text)
+                
+                # 获取机器人的响应
+                response = await conv.get_response(forwarded_message.id)
+              
+                if response.media:
+                    if isinstance(response.media, types.MessageMediaDocument):
+                        mime_type = response.media.document.mime_type
+                        if mime_type.startswith('video/'):
+                            # 处理视频
+                            video = response.media.document
+                            await client.send_file(-1001717350482, video, reply_to=message.id)
+                            print("Forwarded video.")
+                        else:
+                            # 处理文档
+                            document = response.media.document
+                            await client.send_file(-1001717350482, document, reply_to=message.id)
+                            print("Forwarded document.")
+                    elif isinstance(response.media, types.MessageMediaPhoto):
+                        # 处理图片
+                        photo = response.media.photo
+                        await client.send_file(-1001717350482, photo, reply_to=message.id)
+                        print("Forwarded photo.")
+                    else:
+                        print("Received media, but not a document, video, or photo.")
+                elif response.text:
+                    # 处理文本
+                    
+
+                    # 如果机器人的响应是“在您发的这条消息中，没有代码可以被解析”，则将响应发送到 chat_a_id
+                    if response.text == "在您发的这条消息中，没有代码可以被解析":
+                        await self.showfiles(client, message)
+                    elif response.text == "💔抱歉，未找到可解析内容。":
+                        await client.send_message(-1001717350482, response.text, reply_to=message.id)
+                    else:
+                        print("Received text response.")
+
+
+                    
+                    
+
+                    
+                    
+
+                    print("Forwarded text.")
+                else:
+                    print("Received non-media and non-text response")
+
+
+
+                
+
+
+
+        except Exception as e:
+            print(f"\rAn error occurred: {e}\n")
+    
+    async def blgg(self, client, message):
+        bot_username = 'FilesDrive_BLGG_bot'
+        await self.wpbot(client, message, bot_username)
+            
+        
+    
+    async def showfiles(self, client, message):
+        try:
+            bot_username = 'ShowFilesBot'
+            await self.wpbot(client, message, bot_username)
+            
+        except Exception as e:
+            print(f"\rAn error occurred: {e}\n")
     async def datapan(self, client, message):
         try:
-            # 将 message.message 转发给 @datapanbot 机器人
             bot_username = 'datapanbot'
-            print(f"Sending message to {bot_username}...")
-            sent_message = await client.send_message('datapanbot', 'V_DataPanBot_KAkpwpuqF9D6uT56gExZfqc9e1WYV1CiY7M83qpSRZe94drUxmI0oKNXTG')
-            print(f"Sent message to {bot_username}: {sent_message}")
-
-            # 等待机器人的回复
-            @client.on(events.NewMessage(from_users=bot_username))
-            async def handler(event):
-                print(f"Received reply from {bot_username}: {event.message.text}")
-                # 停止监听事件
-                client.remove_event_handler(handler)
+            await self.wpbot(client, message, bot_username)
+            
         except Exception as e:
-            print(f"An error occurred: {e}")
-      
+            print(f"\rAn error occurred: {e}\n")
+
+    async def mediabk(self, client, message):
+        try:
+            bot_username = 'MediaBK2Bot'
+            await self.wpbot(client, message, bot_username)
+            
+        except Exception as e:
+            print(f"\rAn error occurred: {e}\n")
+
+
+        
 
