4d1ccecc455713cdf5a1bf176c930753655efa51
little2 <agile@little2.net>
2024-07-08 20:43:45 +0800

    set last_id

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, sync
 import os
-from vendor.class_bot import LYClass  # 导入 LYClass
+from vendor.class_bot import LYClass,wp_bot,save_last_read_message_id,load_last_read_message_id  # 导入 LYClass
 import asyncio
 import time
 import re
@@ -22,73 +22,7 @@ client = TelegramClient(session_name, api_id, api_hash)
 # 创建 LYClass 实例
 ly_class_instance = LYClass('text')
 
-# 定义机器人信息和正则表达式
-wp_bot = [
-    {
-        'title': 'blgg',
-        'bot_name': 'FilesDrive_BLGG_bot',
-        'id': '6995324980',  # 6854050358
-        'mode': 'enctext',
-        'pattern': r'(?:p_|vi_|f_|fds_|pk_)[a-zA-Z0-9-_]{30,100}\b',
-        'message_thread_id': '23'
-    },
-    {
-        'title': 'filesave',
-        'bot_name': 'FileSaveNewBot',
-        'id': '7008164392',  # 6854050358
-        'mode': 'enctext',
-        'pattern': r'(?:P_|V_|D_)[a-zA-Z0-9-_]{15,29}\b',
-        'message_thread_id': '25'
-    },
-    {
-        'title': 'showfiles',
-        'bot_name': 'ShowFilesBot',
-        'id': '6976547743',  # 6854050358
-        'mode': 'enctext',
-        'pattern': r'(?:showfilesbot_|fds_|pk_)[a-zA-Z0-9-_]{15,29}\b',
-        'message_thread_id': '27'
-    },
-    {
-        'title': 'datapan',
-        'bot_name': 'datapanbot',
-        'id': '6854050358',  # 6854050358
-        'mode': 'enctext',
-        'pattern': r'(?:P_DataPanBot_|V_DataPanBot_|D_DataPanBot_|fds_|pk_)[a-zA-Z0-9-_]{30,100}\b',
-        'message_thread_id': '28'
-    },
-    {
-        'title': 'filetobot',
-        'bot_name': 'filetobot',
-        'id': '291481095',
-        'mode': 'link',
-        'pattern': r'https:\/\/t\.me\/filetobot\?start=(\w{14,20})',
-        'message_thread_id': '29'
-    },
-    {
-        'title': 'filein',
-        'bot_name': 'fileinbot',
-        'id': '1433650553',
-        'mode': 'link',
-        'pattern': r'https:\/\/t\.me\/fileinbot\?start=(\w{14,20})',
-        'message_thread_id': '30'
-    },
-    {
-        'title': 'fileoffrm',
-        'bot_name': 'fileoffrm_bot',
-        'id': '7085384480',
-        'mode': 'link',
-        'pattern': r'https:\/\/t\.me\/fileoffrm_bot\?start=(\w{14,20})',
-        'message_thread_id': '31'
-    },
-    {
-        'title': 'wangpan',
-        'bot_name': 'wangpanbot',
-        'id': '5231326048',
-        'mode': 'link',
-        'pattern': r'https:\/\/t\.me\/WangPanBOT\?start=(file\w{14,20})',
-        'message_thread_id': '32'
-    }
-]
+
 
 # 定义匹配函数
 def match_pattern(input_str):
@@ -101,43 +35,68 @@ def match_pattern(input_str):
 # 定义消息处理函数
 async def process_message(message):
     try:
-        await asyncio.sleep(5)
+       
         # print(f"Processing message: {message}\n")
         print(f"Processing message: {message.id}\n")
 
-        title = match_pattern(message.text)
+        if message.text:
 
-        if title:
-            handler = getattr(ly_class_instance, title, None)
-            if handler:
-                if asyncio.iscoroutinefunction(handler):
-                    await handler(client, message)
+            title = match_pattern(message.text)
+
+            if title:
+                handler = getattr(ly_class_instance, title, None)
+                if handler:
+                    if asyncio.iscoroutinefunction(handler):
+                        await handler(client, message)
+                    else:
+                        handler(message)
                 else:
-                    handler(message)
-            else:
-                print(f"No handler found for title: {title}\n")
+                    print(f"No handler found for title: {title}\n")
         else:
-            print(f"No matching pattern for message: {message.text}\n")
+            print(f"No matching pattern for message: {message.text} {message} \n")
+
+        # 处理完消息后，保存最后读取的消息 ID
+        save_last_read_message_id(message.peer_id.channel_id, message.id)
+
 
     except Exception as e:
-        print(f"An error occurred while processing message: {e}\n")
+        print(f"An error occurred while processing message: {e} {message}\n")
+    finally:
+         await asyncio.sleep(3)
 
 async def main():
     await client.start(phone_number)
 
-    # 使用指定的 chat_id
-    chat_id = -1001507154171
-    ly_class_instance.chat_id = chat_id
+     # 获取并处理频道实体
+    try:
+        chat_id = -1001971784803
+        entity = await client.get_entity(chat_id)
+        ly_class_instance.chat_id = entity.id
+    except ValueError as e:
+        print(f"Failed to get entity for chat_id {chat_id}: {e}")
+        return
+
+    # 加载最后读取的消息 ID
+    last_read_message_id = load_last_read_message_id(entity.id)
+  
+    # 获取最新的消息 ID
+    latest_message = await client.get_messages(entity, limit=1)
+    latest_message_id = latest_message[0].id if latest_message else 0
+
 
     start_time = time.time()
     while True:
-        # 获取最近3条消息
-        async for message in client.iter_messages(chat_id, limit=20):
+        # 获取指定范围内的消息，最多读取20条
+        async for message in client.iter_messages(entity, min_id=last_read_message_id, max_id=latest_message_id, limit=20, reverse=True):
             await process_message(message)
+
+        # 更新最后读取的消息 ID
+        if latest_message_id > last_read_message_id:
+            last_read_message_id = latest_message_id
         
         # 检查累计执行时间是否超过5分钟
         elapsed_time = time.time() - start_time
-        if elapsed_time > 900:  # 300秒等于15分钟
+        if elapsed_time > 900:  # 900秒等于15分钟
             print("Execution time exceeded 5 minutes. Stopping.")
             break
         else:
--- vendor/class_bot.py
+++ vendor/class_bot.py
@@ -1,5 +1,8 @@
 from telethon import types
 import asyncio
+import json
+import os
+
 
 class LYClass:
     def __init__(self, chat_id):
@@ -129,5 +132,91 @@ class LYClass:
         await self.wpbot(client, message, bot_username)        
 
 
- 
+ # 机器人信息和正则表达式定义
+wp_bot = [
+    {
+        'title': 'blgg',
+        'bot_name': 'FilesDrive_BLGG_bot',
+        'id': '6995324980',  # 6854050358
+        'mode': 'enctext',
+        'pattern': r'(?:p_|vi_|f_|fds_|pk_)[a-zA-Z0-9-_]{30,100}\b',
+        'message_thread_id': '23'
+    },
+    {
+        'title': 'filesave',
+        'bot_name': 'FileSaveNewBot',
+        'id': '7008164392',  # 6854050358
+        'mode': 'enctext',
+        'pattern': r'(?:P_|V_|D_)[a-zA-Z0-9-_]{15,29}\b',
+        'message_thread_id': '25'
+    },
+    {
+        'title': 'showfiles',
+        'bot_name': 'ShowFilesBot',
+        'id': '6976547743',  # 6854050358
+        'mode': 'enctext',
+        'pattern': r'(?:showfilesbot_|fds_|pk_)[a-zA-Z0-9-_]{15,29}\b',
+        'message_thread_id': '27'
+    },
+    {
+        'title': 'datapan',
+        'bot_name': 'datapanbot',
+        'id': '6854050358',  # 6854050358
+        'mode': 'enctext',
+        'pattern': r'(?:P_DataPanBot_|V_DataPanBot_|D_DataPanBot_|fds_|pk_)[a-zA-Z0-9-_]{30,100}\b',
+        'message_thread_id': '28'
+    },
+    {
+        'title': 'filetobot',
+        'bot_name': 'filetobot',
+        'id': '291481095',
+        'mode': 'link',
+        'pattern': r'https:\/\/t\.me\/filetobot\?start=(\w{14,20})',
+        'message_thread_id': '29'
+    },
+    {
+        'title': 'filein',
+        'bot_name': 'fileinbot',
+        'id': '1433650553',
+        'mode': 'link',
+        'pattern': r'https:\/\/t\.me\/fileinbot\?start=(\w{14,20})',
+        'message_thread_id': '30'
+    },
+    {
+        'title': 'fileoffrm',
+        'bot_name': 'fileoffrm_bot',
+        'id': '7085384480',
+        'mode': 'link',
+        'pattern': r'https:\/\/t\.me\/fileoffrm_bot\?start=(\w{14,20})',
+        'message_thread_id': '31'
+    },
+    {
+        'title': 'wangpan',
+        'bot_name': 'wangpanbot',
+        'id': '5231326048',
+        'mode': 'link',
+        'pattern': r'https:\/\/t\.me\/WangPanBOT\?start=(file\w{14,20})',
+        'message_thread_id': '32'
+    }
+]
         
+# 持久化存储最后读取的消息 ID
+LAST_READ_MESSAGE_FILE = "last_read_message_id.json"
+
+def save_last_read_message_id(chat_id, message_id):
+    data = {str(chat_id): message_id}
+    if os.path.exists(LAST_READ_MESSAGE_FILE):
+        with open(LAST_READ_MESSAGE_FILE, 'r') as file:
+            existing_data = json.load(file)
+        existing_data.update(data)
+        data = existing_data
+
+    with open(LAST_READ_MESSAGE_FILE, 'w') as file:
+        json.dump(data, file)
+
+def load_last_read_message_id(chat_id):
+    if os.path.exists(LAST_READ_MESSAGE_FILE):
+        with open(LAST_READ_MESSAGE_FILE, 'r') as file:
+            data = json.load(file)
+            return data.get(str(chat_id), 0)  # 返回 0 作为默认值
+    return 0
