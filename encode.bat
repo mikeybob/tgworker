3c139cc7e68f11176d53860f6c43eaf528a1edce
little2 <agile@little2.net>
2024-08-07 14:31:04 +0800

    remove

--- encode.bat
+++
@@ -1,2 +0,0 @@
-D:\"Program Files\OpenSSL-Win64"\bin\openssl.exe aes-256-cbc -pbkdf2 -salt -in 27437823session_name.session -out 27437823session_name.session.enc -pass pass:2cd52bc41b1196baab4a32e04c376dcb2cd52bc41b1196baab4a32e04c376dcb
-D:\"Program Files\OpenSSL-Win64"\bin\openssl.exe aes-256-cbc -pbkdf2 -salt -in 29614663session_name.session -out 29614663session_name.session.enc -pass pass:2cd52bc41b1196baab4a32e04c376dcb2cd52bc41b1196baab4a32e04c376dcb
