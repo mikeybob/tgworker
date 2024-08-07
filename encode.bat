1676843382da45de131a8b1aed9e7f64dde7a6cc
little2 <agile@little2.net>
2024-08-07 14:14:15 +0800

    0807

--- encode.bat
+++ encode.bat
@@ -1,18 +1,2 @@
-@echo off
-@set PATH=%PATH%;%~dp0bin
-
-title Win64 OpenSSL Command Prompt
-echo Win64 OpenSSL Command Prompt
-echo.
-openssl version -a
-echo.
-
-%SystemDrive%
-cd %UserProfile%
-
-cmd.exe /K
-
-@REM e:
-@REM cd E:\OneDrive\Code\tgworker
-openssl aes-256-cbc -pbkdf2 -salt -in 27437823session_name.session -out 27437823session_name.session.enc -pass pass:2cd52bc41b1196baab4a32e04c376dcb2cd52bc41b1196baab4a32e04c376dcb
-openssl aes-256-cbc -pbkdf2 -salt -in 29614663session_name.session -out 29614663session_name.session.enc -pass pass:2cd52bc41b1196baab4a32e04c376dcb2cd52bc41b1196baab4a32e04c376dcb
+D:\"Program Files\OpenSSL-Win64"\bin\openssl.exe aes-256-cbc -pbkdf2 -salt -in 27437823session_name.session -out 27437823session_name.session.enc -pass pass:2cd52bc41b1196baab4a32e04c376dcb2cd52bc41b1196baab4a32e04c376dcb
+D:\"Program Files\OpenSSL-Win64"\bin\openssl.exe aes-256-cbc -pbkdf2 -salt -in 29614663session_name.session -out 29614663session_name.session.enc -pass pass:2cd52bc41b1196baab4a32e04c376dcb2cd52bc41b1196baab4a32e04c376dcb
